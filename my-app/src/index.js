fa28cc092477e7fa9549af23d4cd442efcc93550
Cloudy_Place <minhnnm1009@gmail.com>
2023-05-10 21:41:55 +0700

    backroads-app project

---
+++ my-app/src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
+    alt="hihih"
+  />
+);
+const Title = () => {
+  return <h2>Book Title</h2>;
+};
+const Author = () => <h4>Author</h4>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
